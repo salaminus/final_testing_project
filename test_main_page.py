1076eb71d141835a5196866b32e51bebd3bc4c57
rootmaster <salaminus@gmail.com>
2025-01-20 21:05:50 +0300

    Create symple example

---
+++ test_main_page.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+
+# run:
+# pytest.exe -v --tb=line test_main_page.py
