64f437831f710522b3b9d7949d350f8759862487
rootmaster <salaminus@gmail.com>
2025-01-21 13:08:35 +0300

    Create symple example #5-1. Find login and reg forms.

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,7 @@
 import time
 
-from selenium.webdriver.common.by import By
+from .login_page import LoginPage
 from .base_page import BasePage
-
 from .locators import MainPageLocators
 
 
@@ -10,7 +9,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        time.sleep(2)
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
 
 
     def should_be_login_link(self):
--- test_main_page.py
+++ test_main_page.py
@@ -3,17 +3,24 @@ from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/"
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    login_link = 'https://selenium1py.pythonanywhere.com/ru/accounts/login/'
-    # page = MainPage(browser, link)
-    # page.open()
+    # login_link = 'https://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+    # Or (Into the MainPage need delete return!!!):
+    # login_page = LoginPage(browser, browser.current_url)
+    # login_page.should_be_login_page()
+
+    # Old:
     # page.go_to_login_page()
     # page.should_be_login_link()
-    login_page = LoginPage(browser, login_link)
-    login_page.should_be_login_url()
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
+    # login_page = LoginPage(browser, login_link)
+    # login_page.should_be_login_url()
+    # login_page.should_be_login_form()
+    # login_page.should_be_register_form()
 
 
 # run:
