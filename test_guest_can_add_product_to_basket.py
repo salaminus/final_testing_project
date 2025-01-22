ac6e2d7f06ac3fc454a30612c97c885f28a54042
rootmaster <salaminus@gmail.com>
2025-01-22 12:24:39 +0300

    Create symple example #6. Many tests.

--- test_guest_can_add_product_to_basket.py
+++ test_guest_can_add_product_to_basket.py
@@ -7,7 +7,8 @@ from .pages.cart_page import CartPage
 
 # With parametrizing
 @pytest.mark.parametrize('link', [
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
+    # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
     # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
     # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -18,19 +19,42 @@ from .pages.cart_page import CartPage
     # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
 ])
-def test_add_product_in_basket(browser, link):
-    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+# def test_add_product_in_basket(browser, link):
+#     # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+#     # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
+#     page.should_be_add_product()
+#     page.solve_quiz_and_get_code()
+#     page.should_go_cart()
+#     page.should_name_product_in_cart()
+#     cart_page = page.should_name_product_in_cart()
+#     product_name_added = cart_page.should_be_added_product()
+#     # print(f"Продукт в корзине: {product_name_added}")
+#     assert page.name_product == product_name_added, "Не совпадает!!!"
+#     page.should_not_be_success_message_until_not()
+#     time.sleep(50)
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    # time.sleep(30)
+    page.should_be_add_product()
+    page.solve_quiz_and_get_code()
+    time.sleep(30)
+    page.is_not_element_present()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+def test_guest_cant_see_success_message(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+def test_message_disappeared_after_adding_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
     page.should_be_add_product()
-    page.solve_quiz_and_get_code()
-    page.should_go_cart()
-    page.should_name_product_in_cart()
-    cart_page = page.should_name_product_in_cart()
-    product_name_added = cart_page.should_be_added_product()
-    # print(f"Продукт в корзине: {product_name_added}")
-    assert page.name_product == product_name_added, "Не совпадает!!!"
     page.should_not_be_success_message_until_not()
-    time.sleep(50)
